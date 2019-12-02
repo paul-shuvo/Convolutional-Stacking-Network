5a274ccf1757db901eb8d0ef3698b226f0dba680
Pourya Hoseini <sph.alinodehi@gmail.com>
2019-12-01 18:16:44 -0800

    1- Inference function is added. 2- Kernel PCA is added. 3- The ability to use feature extractors directly, instead of trained kernels, is added. 4- More configurations are added in the config file. 5- Some other minor adjustments.

--- Run.py
+++ Run.py
@@ -1,7 +1,9 @@
-from PCA_Net import PCA_Net
+from PCA_Net import PCA_Net, Kernel_PCA_Net
 
 # Define an instance of stacking PCA convolutional network
-pcaNet = PCA_Net('Config.cfg', configSection="DEFAULT")
+pcaNet = PCA_Net('Config.cfg', config_section="DEFAULT")
 
 # Train the stacking convolutional network
 pcaNet.train()
+
+b=pcaNet.infer(pcaNet.test_samples, pcaNet.feature_extractors, pcaNet.network_settings, False)
